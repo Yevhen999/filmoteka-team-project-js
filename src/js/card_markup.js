7a307e50f23f84d63f3396c74d84713a4d8eb81b
Yevhen <leshchenkoyevhen@gmail.com>
2022-11-09 16:37:29 -0500

    add default poster

--- src/js/card_markup.js
+++ src/js/card_markup.js
@@ -1,7 +1,13 @@
 import { createStringOfGenresForCard } from './genres_storage';
 
 export function createCardMarkup(data) {
-  const { title, poster_path, release_date, genre_ids, id } = data;
+  const {
+    title,
+    poster_path = 'https://www.bworldonline.com/wp-content/uploads/2022/04/cinema02_14-01.jpg',
+    release_date,
+    genre_ids,
+    id,
+  } = data;
   let date = release_date;
   if (!date) {
     date = 'No information';
@@ -9,11 +15,11 @@ export function createCardMarkup(data) {
     date = release_date.slice(0, 4);
   }
 
-  let imageUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`;
-  if (poster_path === null) {
-    imageUrl =
-      'https://www.bworldonline.com/wp-content/uploads/2022/04/cinema02_14-01.jpg';
-  }
+  // let imageUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+  // if (poster_path === null) {
+  //   imageUrl =
+  //     'https://www.bworldonline.com/wp-content/uploads/2022/04/cinema02_14-01.jpg';
+  // }
 
   let cardGenres;
   if (!genre_ids) {
@@ -25,7 +31,7 @@ export function createCardMarkup(data) {
   return `
     <li class="card__container" data-id="${id}">
     <a class="link card-film-link" href="#" aria-label="${title}">
-    <img class="card__poster" alt='poster' data-id="${id}" src="${imageUrl}" height="574"  width="395"/>
+    <img class="card__poster" alt='poster' data-id="${id}" src="https://image.tmdb.org/t/p/w500/${poster_path}" height="574"  width="395"/>
     <div class="description-wrapper">
     <b class="card__film-name">${title}</b>
     <p class="card__description">${cardGenres}<span class="film-year">${date}</span>
